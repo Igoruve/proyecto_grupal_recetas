80c91065c5f032648a26ef39f5cb7c0412670de4
Carlos <carlosborjawebdev@gmail.com>
2025-03-25 10:44:22 +0100

    add functional favorites system

--- scripts/localStorageUtils.js
+++ scripts/localStorageUtils.js
@@ -1,3 +1,5 @@
+import { fetchMealById } from "./functions.js";
+
 export const getFavArray = () => {
     if(JSON.parse(localStorage.getItem("favs"))==null){
       return [];
@@ -35,3 +37,28 @@ export const getFavArray = () => {
     button.textContent = checkIfInFavArray(id) ? "★ Remove from Favorites" : "☆ Add to Favorites";
     button.classList.toggle("favorited", checkIfInFavArray(id));
   }
+
+  export const getFavRecipes = async () =>{
+    const favList = getFavArray();
+    console.log("La lista de favs es", favList);
+    if (favList.length==0){
+      return [];
+    }else{
+      const favRecipes = await Promise.all(favList.map(async(id)=> {
+        return fetchMealById(id);
+      }));
+      console.log("Las recetas en si son: ", favRecipes);
+      return favRecipes;
+    }
+  }
+
+  // export async function fetchMealById(mealId) {
+  //   try {
+  //     const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
+  //     const data = await res.json();
+  //     return data.meals ? data.meals[0] : null;
+  //   } catch (error) {
+  //     console.error('Error obteniendo receta:', error);
+  //     return null;
+  //   }
+  // }
